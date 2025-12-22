550430c84b4fe8f7fe6893a34b70e6f524ab7965
NNNNJJHGGB <106750663+NNNNJJHGGB@users.noreply.github.com>
2025-12-22 23:40:10 +0100

    items

---
+++ src/main/java/net/karrot/fnafone/item/ModItems.java
@@ -0,0 +1,10 @@
+package net.karrot.fnafone.item;
+
+import net.karrot.fnafone.FnafOne;
+
+public class ModItems {
+
+    public static void registerModItems() {
+        FnafOne.LOGGER.info("Registering Mod Items for " + FnafOne.MOD_ID);
+    }
+}
