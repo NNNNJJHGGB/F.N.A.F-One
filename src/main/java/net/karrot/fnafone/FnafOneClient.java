b72051d1fa610eab4d374742bfcf31a1410c5904
NNNNJJHGGB <106750663+NNNNJJHGGB@users.noreply.github.com>
2025-12-22 23:33:46 +0100

    Initial commit

---
+++ src/main/java/net/karrot/fnafone/FnafOneClient.java
@@ -0,0 +1,10 @@
+package net.karrot.fnafone;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FnafOneClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/karrot/fnafone/FnafOneDataGenerator.java
@@ -0,0 +1,11 @@
+package net.karrot.fnafone;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FnafOneDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
